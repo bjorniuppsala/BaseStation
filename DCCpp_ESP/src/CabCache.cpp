ddf06187319705a0005cda593d3ac0db67d588e2
Björn Bergman <bjornbergman@gmail.com>
2017-12-15 21:57:49 +0100

    Add more logging to CabCache and fix null pointers on bad json.

--- DCCpp_ESP/src/CabCache.cpp
+++ DCCpp_ESP/src/CabCache.cpp
@@ -3,11 +3,15 @@
 #include "DCCpp_ESP.h"
 #include "ArduinoJson.h"
 #include "AsyncJson.h"
+#include "StreamString.h"
 #include <algorithm>
 
 CabCache::CabCache(String const& url)
 : mWebsocket { url }
-{}
+{
+	mWebsocket.onEvent([this](auto server, auto client, auto type, auto arg, auto data, auto len)
+		{handleReq(server, client, type, arg, data, len);});
+}
 void CabCache::hookUp(AsyncWebServer& server)
 {
 	server.addHandler(&mWebsocket);
@@ -45,19 +49,26 @@ void CabCache::handleReq(AsyncWebSocket * server, AsyncWebSocketClient * client,
 	switch(type) {
 		case WS_EVT_CONNECT:
 			mClientBuf[client->id()] = {};
+			Serial.printf("[WS] new client %d\n", client->id());
 			break;
 		case WS_EVT_DISCONNECT:
 			mClientBuf.erase(client->id());
+			Serial.printf("[WS] disconnect client %d\n", client->id());
 			break;
 		case WS_EVT_PONG:
+			Serial.printf("[WS] pong client %d\n", client->id());
 			break;
 		case WS_EVT_ERROR:
+			Serial.printf("[WS] error on client %d\n", client->id());
 			//well..
 			break;
 		case WS_EVT_DATA:
 			auto info = reinterpret_cast<AwsFrameInfo const*>(arg);
 			auto& buffer = mClientBuf[client->id()];
 
+			Serial.printf("[WS] client data %d\n", client->id());
+			Serial.printf("[WS]   info: index %d len %d final: %d part len %d\n",
+			                            (int)info->index, (int)info->len, (int)info->final, len);
             // First packet
             if (info->index == 0) {
                 if(info->len > MAX_WEBSOCKET_DATA_SIZE)
@@ -73,6 +84,7 @@ void CabCache::handleReq(AsyncWebSocket * server, AsyncWebSocketClient * client,
 
             // Last packet
             if (info->index + len == info->len && info->final) {
+				Serial.printf("Found fill packet '%s'\n", buffer.c_str());
                 handleReq(client, buffer);
                 buffer.clear();
             }
@@ -83,16 +95,19 @@ void CabCache::handleReq(AsyncWebSocketClient * client, std::string& message)
 {
 	DynamicJsonBuffer buffer;
 	auto& json = buffer.parseObject(const_cast<char*>(message.c_str()));
-	if(json["type"] == "command") {
+	char const* type = json["type"];
+	if(!type) return;
+	else if(strcmp(type, "command") == 0) {
 		String msg = json["command"];
 		if(msg.length() > 0);
 		DCCpp::Server::pushPendingDCCCommand(std::move(msg));
-	} else if(json["type"] == "speed"){
-		StreamString msg;
-		int speed = json["speed"];
+	} else if(strcmp(type, "speed") == 0){
+		//{ "type" : "speed", "register":"1", "id":"3", "speed":"4"}
+		int speed = json["speed"].as<int>();
 		int direction = speed < 0 ? 0 : 1;
 		speed = abs(speed);
-		msg.printf(PSTR("<t %d %d %d %d>"), json["register"], json["address"], speed, direction);
+		StreamString msg;
+		msg.printf(PSTR("<t %d %d %d %d>"), json["register"].as<int>(), json["id"].as<int>(), speed, direction);
 		DCCpp::Server::pushPendingDCCCommand(std::move(msg));
 	}
 
@@ -106,7 +121,7 @@ void CabCache::loadFrom(fs::FS& fs, char const* path)
 	DynamicJsonBuffer buf;
 	auto& json = buf.parseObject(f);
 	for(auto const& c : json) {
-		Serial.printf("considering: %s ", c.key);
+		Serial.printf("loading: %s ", c.key);
 		c.value.printTo(Serial);
 		Serial.printf("\n");
 		auto& u = c.value.as<JsonObject const&>();
