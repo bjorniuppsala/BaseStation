47407ef2ec28fc5ab3b67a319f0dc9caa3b09a99
Björn Bergman <bjornbergman@gmail.com>
2017-12-31 00:07:35 +0100

    Make sure that the final one (stop) bit is added to teh buffer.
    Previously this worked because we had interrupts triggering and puttnig out the next bit, but with RMT generation there may be a slight glitch after each packet.

--- DCCpp_Uno/src/PacketRegister.cpp
+++ DCCpp_Uno/src/PacketRegister.cpp
@@ -69,24 +69,24 @@ void RegisterList::loadPacket(int nReg, byte *b, int nBytes, int nRepeat, int pr
   buf[6]=b[2]<<7;                     // b[2], bit 0
 
   if(nBytes==3){
-    p->nBits=49;
+    p->nBits=50;
 	buf[6] |= 1 << 6;
   } else{
     buf[6]+=b[3]>>2;                  // b[3], bits 7-2
     buf[7]=b[3]<<6;                   // b[3], bit 1-0
     if(nBytes==4){
-      p->nBits=58;
+      p->nBits=59;
 	  buf[7] |= 1 << 5;
     } else{
       buf[7]+=b[4]>>3;                // b[4], bits 7-3
       buf[8]=b[4]<<5;                 // b[4], bits 2-0
       if(nBytes==5){
-        p->nBits=67;
+        p->nBits=68;
 		buf[8] |= 1 << 4;
       } else{
         buf[8]+=b[5]>>4;              // b[5], bits 7-4
         buf[9]=b[5]<<4;               // b[5], bits 3-0
-        p->nBits=76;
+        p->nBits=77;
 		buf[9] |= 1 << 3;
       } // >5 bytes
     } // >4 bytes
