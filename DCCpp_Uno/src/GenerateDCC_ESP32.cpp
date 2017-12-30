f68cd4bfcd2e162ca683ab8ea65d77afcbf40c0b
Björn Bergman <bjornbergman@gmail.com>
2017-12-31 00:05:49 +0100

    Generate DCC using the RMT module.
    Uses one task to keep the RMT buffer full not ideal perhaps but works nicely.

--- DCCpp_Uno/src/GenerateDCC_ESP32.cpp
+++ DCCpp_Uno/src/GenerateDCC_ESP32.cpp
@@ -6,120 +6,60 @@
 #include <array>
 #include <atomic>
 #include <Arduino.h>
+#include <driver/rmt.h>
 constexpr uint16_t TIMER_DIVISOR = 80;  //1MHz (1us) timer count
 constexpr uint64_t ZERO_PERIOD = 100;    // 58us
 constexpr uint64_t ONE_PERIOD = 58;
-#include <driver/rmt.h>
+
 // each half-pulse should be 58us for a one and 100us for a zero
 
 namespace GenerateDCC{
     namespace {
-		#define constexpr
-		constexpr rmt_item32_t createBit(uint16_t duration)
+		constexpr rmt_item32_t createBit(uint32_t duration)
 		{
-			rmt_item32_t item;
-			item.duration0 = item.duration1 = duration;
-			item.level0 = 1; item.level1 = 0;
+			rmt_item32_t item ;//{ duration, duration, 1, 0};
+			item.val = (duration << 16) | duration | 1 << 15;
 			return item;
 		}
-		constexpr rmt_item32_t zero_bit = createBit(ZERO_DURATION);
-		constexpr rmt_item32_t one_bit = createBit(ONE_DURATION);
+		constexpr rmt_item32_t zero_bit = createBit(ZERO_PERIOD);
+		constexpr rmt_item32_t one_bit = createBit(ONE_PERIOD);
 		constexpr uint16_t tx_buf_size = 128;
 		struct dcc_generator_t {
-			uint16_t tx_buf_fillstart;   // the index i which we should starat filling. index into rmt_item32_t i.e. one bit
-			uint16_t tx_buf_intr_threshold; // the index at which the interrupt (wil) happen
-			uint16_t last_tx_intr_handled;
 		    rmt_channel_t channel;
 			RegisterList volatile* packets;
 			/*std::atomic<uint16_t>*/ uint16_t tx_intr_count;
 		};
 
 		dcc_generator_t p_rmt_obj[RMT_CHANNEL_MAX] = {};
-		rmt_isr_handle_t s_dcc_driver_intr_handle;
-		inline auto moveBufIndex(uint16_t src, uint16_t steps)
-		{
-			return (src + steps) % tx_buf_size;
-		}
-		inline void fill_with_bits(dcc_generator_t* p_rmt, uint16_t start, uint16_t end, rmt_item32_t bit)
-		{
-			for(auto i = start; i < end; i++) {
-		        const_cast<rmt_item32_t&>(RMTMEM.chan[p_rmt->channel].data32[i]) = bit;
-		    }
-		}
 
-		void IRAM_ATTR dcc_gen_rmt_isr(void* arg)
-		{
-		    uint32_t intr_st = RMT.int_st.val;
-		    uint32_t i = 0;
-		    uint8_t channel;
-		    portBASE_TYPE HPTaskAwoken = 0;
-		    for(i = 0; i < 32; i++) {
-		        if(i < 24) {
-		            if(intr_st & BIT(i)) {
-		                channel = i / 3;
-		                dcc_generator_t* p_rmt =&p_rmt_obj[channel];
-		                switch(i % 3) {
-		                    //TX END
-		                    case 0:
-								p_rmt->tx_intr_count++;
-		                        break;
-		                        //RX_END
-		                    case 1:
-		                        break;
-		                        //ERR
-		                    case 2:
-		                        RMT.int_ena.val &= (~(BIT(i)));
-		                        break;
-		                    default:
-		                        break;
-		                }
-		                RMT.int_clr.val = BIT(i);
-		            }
-		        } else {
-		            if(intr_st & (BIT(i))) {
-		                channel = i - 24;
-		                RMT.int_clr.val = BIT(i);
-						dcc_generator_t* p_rmt = &p_rmt_obj[channel];
-						p_rmt->tx_intr_count++;
-		            }
-		        }
-		    }
-		}
 
-		void do_fill_buffer(dcc_generator_t* p_rmt, uint16_t start, uint16_t end)
+		void fillRMTTask(void* arg)
 		{
-			auto* data32 = const_cast<rmt_item32_t*>(RMTMEM.chan[p_rmt->channel].data32);
-			for(auto p = start; p < end; ++p)
-			{
-				data32[p] = p_rmt->packets->NextBit() ? one_bit :zero_bit;
+			auto gen = reinterpret_cast<dcc_generator_t*>(arg);
+			rmt_item32_t bits_to_send[128];
+			std::fill(std::begin(bits_to_send), std::end(bits_to_send), zero_bit);
+			auto n_bits = 128;
+			while(1) { // This may look backwards, but by sending, setting up the next
+				// and then waiting for tx to be done we improve timing a little bit.
+				//Remember that the signal stays still while we are not sending...
+				auto err = rmt_write_items(gen->channel, bits_to_send, n_bits, false);
+				if(err != ESP_OK) {
+					Serial.printf("rmt_write_items failed: %d channel: %d\n", err, gen->channel);
+				}
+				auto p = gen->packets->NextPacket();
+				//Serial.printf("DCC: %d nbits %d\n", gen->channel, p->nBits);
+				if(p->nBits > 120) continue; // this will repeat the same packet again...
+				for(int b = 0; b < p->nBits; ++b) {
+					auto v = p->buf[b/8] & RegisterList::bitMask[b%8] ;
+					bits_to_send[b] = v ? one_bit : zero_bit;
+				}
+				bits_to_send[p->nBits] = one_bit; // push an extra on_bit to ensure that the packet finishs nicely.
+				n_bits = p->nBits + 1;
+				err = rmt_wait_tx_done(gen->channel, portMAX_DELAY);
+				if(err != ESP_OK) {
+					Serial.printf("rmt_wait_tx_done failed: %d channel: %d\n", err, gen->channel);
+				}
 			}
-		}
-		void try_fill_buffer(dcc_generator_t* p_rmt)
-		{
-			uint32_t status = 0;
-			rmt_get_status(p_rmt->channel, &status);
-			Serial.printf("try_fill: intr_count %d last %d status: 0x%x apb addr: 0x%x data_ch 0x%x\n",
-				p_rmt->tx_intr_count, p_rmt->last_tx_intr_handled,
-				status, RMT.apb_mem_addr_ch[p_rmt->channel], RMT.data_ch[p_rmt->channel]);
-			if(p_rmt->tx_intr_count == p_rmt->last_tx_intr_handled)
-				return;
-			p_rmt->last_tx_intr_handled = p_rmt->tx_intr_count;
-
-			auto fill_end = p_rmt->tx_buf_intr_threshold;
-			auto to_fill = (fill_end- p_rmt->tx_buf_fillstart) % tx_buf_size;
-			p_rmt->tx_buf_intr_threshold = moveBufIndex(fill_end, to_fill);
-			Serial.printf("Filling: fill_start %d, fill_end %d, next thresh %d\n", p_rmt->tx_buf_fillstart, fill_end, p_rmt->tx_buf_intr_threshold);
-			if(fill_end < p_rmt->tx_buf_fillstart)
-			{ //fill up to end of block:
-				do_fill_buffer(p_rmt, p_rmt->tx_buf_fillstart, tx_buf_size);
-				p_rmt->tx_buf_fillstart = 0;
-			}
-			do_fill_buffer(p_rmt, p_rmt->tx_buf_fillstart, fill_end);
-			Serial.printf("Filling:Buffer done.\n");
-			p_rmt->tx_buf_fillstart = fill_end;
-			//rmt_set_tx_thr_intr_en(p_rmt->channel, true, p_rmt->tx_buf_intr_threshold);
-			RMT.tx_lim_ch[p_rmt->channel].limit = p_rmt->tx_buf_intr_threshold;
-			Serial.printf("Filling:Done\n");
 		}
     }
 
@@ -138,50 +78,45 @@ namespace GenerateDCC{
 		p_rmt_obj[0].packets = &mainRegs;
 		p_rmt_obj[1].packets = &progRegs;
 
-		rmt_config_t c1_config = {
-			/*.rmt_mode =*/ RMT_MODE_TX,
-			/*.channel = */RMT_CHANNEL_0,
-			/*.clk_div = */TIMER_DIVISOR,
-			/*.gpio_num = */(gpio_num_t)2,
-			/*.mem_block_num = */(uint8_t)(tx_buf_size / 64), // have one rtmem block of 64 32 bit words. (space for 128 bits)
-			/*.tx_config =*/ {
-				/*.loop_en =*/ false,          /*!< RMT loop output mode*/
-    			/*.carrier_freq_hz =*/ 0,      /*!< RMT carrier frequency */
-    			/*.carrier_duty_percent =*/ 0, /* RMT carrier duty (%) */
-     			/*.carrier_level =*/ RMT_CARRIER_LEVEL_HIGH,  /*!< RMT carrier level */
-    			/*.carrier_en =*/ false,       /*!< RMT carrier enable */
-    			/*.idle_level = */RMT_IDLE_LEVEL_LOW, /*!< RMT idle level */
-				/*.idle_output_en = */false
-			}
-		};
+		for(int c = 0; c < 2; ++c) {
+			rmt_config_t c1_config = {
+				/*.rmt_mode =*/ RMT_MODE_TX,
+				/*.channel = */c == 0 ? RMT_CHANNEL_0 : RMT_CHANNEL_4,
+				/*.clk_div = */TIMER_DIVISOR,
+				/*.gpio_num = */(gpio_num_t)(c == 0 ? DCC_SIGNAL_PIN_MAIN : DCC_SIGNAL_PIN_PROG),
+				/*.mem_block_num = */(uint8_t)(2), // have two rtmem block of 64 32 bit words. (space for 128 bits)
+				/*.tx_config =*/ {
+					/*.loop_en =*/ false,          /*!< RMT loop output mode*/
+	    			/*.carrier_freq_hz =*/ 0,      /*!< RMT carrier frequency */
+	    			/*.carrier_duty_percent =*/ 0, /* RMT carrier duty (%) */
+	     			/*.carrier_level =*/ RMT_CARRIER_LEVEL_HIGH,  /*!< RMT carrier level */
+	    			/*.carrier_en =*/ false,       /*!< RMT carrier enable */
+	    			/*.idle_level = */RMT_IDLE_LEVEL_LOW, /*!< RMT idle level */
+					/*.idle_output_en = */false
+				}
+			};
+			p_rmt_obj[c].channel = c1_config.channel;
+			rmt_config(&c1_config);
+			auto intr_alloc_flags = ESP_INTR_FLAG_IRAM | ESP_INTR_FLAG_LOWMED | ESP_INTR_FLAG_LEVEL3;
+			auto err = rmt_driver_install(c1_config.channel, 0, 0);
+			if(err != ESP_OK) Serial.printf("Failed to install driver: %d\n", err);
+			char tn[] = "FILLRMT0";
+			tn[8] += c;
+			// spawn a task
+			err = xTaskCreate(    &fillRMTTask,
+	                            tn,
+								5* 1024,
+	                            p_rmt_obj + c,
+	                            1, //UBaseType_t uxPriority,
+	                            nullptr//TaskHandle_t *pxCreatedTask
+	                          );
 
-		rmt_config(&c1_config);
-		auto intr_alloc_flags = ESP_INTR_FLAG_IRAM | ESP_INTR_FLAG_LOWMED | ESP_INTR_FLAG_LEVEL3;
-		auto err = rmt_isr_register(dcc_gen_rmt_isr, NULL, intr_alloc_flags, &s_dcc_driver_intr_handle);
-		if(err != ESP_OK) Serial.printf("Failed to install isr: %d\n", err);
-
-		for(int i = 0; i < 1; ++i)
-		{
-			auto& g = p_rmt_obj[i];
-			g.tx_buf_fillstart = 0;
-			g.tx_buf_intr_threshold = 32;
-			g.tx_intr_count = 0;
-			g.last_tx_intr_handled =  0;
-			g.channel = (rmt_channel_t)i;
-			RMT.apb_conf.fifo_mask = RMT_DATA_MODE_MEM;
-			do_fill_buffer(&g, 0, tx_buf_size);
-			rmt_set_tx_thr_intr_en(g.channel, true, g.tx_buf_intr_threshold);
-			err = rmt_set_tx_intr_en(g.channel, 1);
-			if(err != ESP_OK) Serial.printf("Failed to rmt_set_tx_intr_en: %d\n", err);
-			err = rmt_set_tx_thr_intr_en(g.channel, 1, g.tx_buf_intr_threshold
-			);
-			if(err != ESP_OK) Serial.printf("Failed to rmt_set_tx_thr_intr_en: %d\n", err);
-			rmt_tx_start(g.channel, true);
+			if(err != ESP_OK) Serial.printf("Failed to start task driver: %d\n", err);
 		}
     }
     void loop()
     {
-		try_fill_buffer(&p_rmt_obj[0]);
+	//	try_fill_buffer(&p_rmt_obj[0]);
 	}
 
 }
--- DCCpp_Uno/src/PacketRegister.h
+++ DCCpp_Uno/src/PacketRegister.h
@@ -60,6 +60,28 @@ struct RegisterList{
   void writeCVBitMain(const char *s) volatile;
   void printPacket(int, byte *, int, int) volatile;
 
+  auto NextPacket() volatile
+  {
+	  auto& R = *this;
+	  auto packet = R.currentReg->activePacket;
+	  R.currentBit=0;                           /*   reset current bit pointer and determine which Register and Packet to process next--- */
+	  if(R.nRepeat>0 && R.currentReg==R.reg){               /*   IF current Register is first Register AND should be repeated */
+	    R.nRepeat--;                                        /*     decrement repeat count; result is this same Packet will be repeated */
+	  } else if(R.nextReg!=NULL){                           /*   ELSE IF another Register has been updated */ \
+	    R.currentReg=R.nextReg;                             /*     update currentReg to nextReg */ \
+	    R.nextReg=NULL;                                     /*     reset nextReg to NULL */ \
+	    auto tmp =R.currentReg->activePacket;            /*     flip active and update Packets */ \
+	    packet = R.currentReg->activePacket = R.currentReg->updatePacket; \
+	    R.currentReg->updatePacket=tmp; \
+	  } else{                                               /*   ELSE simply move to next Register */ \
+	    if(R.currentReg==R.maxLoadedReg)                    /*     BUT IF this is last Register loaded */ \
+	  	R.currentReg=R.reg;                               /*       first reset currentReg to base Register, THEN */ \
+	    R.currentReg++;
+	    packet = R.currentReg->activePacket;/*     increment current Register (note this logic causes Register[0] to be skipped when simply cycling through all Registers) */ \
+	  }
+	  return packet;
+  }
+
   inline auto NextByte() volatile
   {
 	  auto& R = *this;
